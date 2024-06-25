e43bf0cb9a7a8bc9e5349e38cc604be235654de3
Ranul_112445 <ranul.gamage@axiatadigitallabs.com>
2024-06-25 09:41:40 +0530

    section 01 completed how to install and configure TS also how debug with typescript

---
+++ Section 01/hello-world/dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+console.log("hello world!!!!");
+let age = 22;
+let bornYearis = 2002;
+age + 10;
+console.log(age);
+age = 50;
+console.log(age);
+bornYearis = Math.floor(Math.random() * (2002) + 1998);
+if (bornYearis >= 2002) {
+    age = 22;
+}
+else if (bornYearis >= 1998) {
+    age = 26;
+}
+console.log(age);
+//# sourceMappingURL=index.js.map
